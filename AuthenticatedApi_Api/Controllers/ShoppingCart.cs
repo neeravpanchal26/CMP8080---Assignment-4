e04a450090f9d39bd0b23af892fd6c522e6a3a95
Neerav Panchal <neeravpanchal26@gmail.com>
2024-02-23 01:34:45 +0000

    Add a Get endpoint that returns all products in the user's shopping cart.

--- AuthenticatedApi_Api/Controllers/ShoppingCart.cs
+++ AuthenticatedApi_Api/Controllers/ShoppingCart.cs
@@ -3,16 +3,37 @@ using IdentityAPI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.OpenApi.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace AuthenticatedApi_Api;
-
-[Authorize]
-[ApiController]
-[Route("[controller]")]
-public class ShoppingCartController : ControllerBase
+namespace AuthenticatedApi_Api
 {
-    public ShoppingCartController()
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class ShoppingCartController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ShoppingCartController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetShoppingCart()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+
+            var shoppingCart = await _context.ShoppingCarts
+                .Include(cart => cart.Products)
+                .FirstOrDefaultAsync(cart => cart.UserId == currentUser.Id);
+
+            return Ok(shoppingCart.Products);
+        }
     }
 }
